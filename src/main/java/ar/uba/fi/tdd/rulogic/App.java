90d7d4ac1aad752d46ebc808adb0d22d3c5b09ff
Pablo Murck <pmurck@baufest.com>
2017-11-05 21:49:15 -0300

    Implement interactive console App

--- src/main/java/ar/uba/fi/tdd/rulogic/App.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/App.java
@@ -1,12 +1,45 @@
 package ar.uba.fi.tdd.rulogic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import ar.uba.fi.tdd.rulogic.exception.NonExistentMethodException;
+import ar.uba.fi.tdd.rulogic.exception.RecursiveRuleCallException;
+import ar.uba.fi.tdd.rulogic.exception.RuleJoinException;
+import ar.uba.fi.tdd.rulogic.exception.WrongSyntaxException;
 /**
  * Console application.
  *
  */
-public class App
-{
+public class App {
+	
 	public static void main(String[] args) {
-		System.out.println("I shall answer all your questions!");
+		
+		if (args.length != 1) {
+			System.out.println("You must Specify ONE database filename as argument!");
+			return;
+		}
+		
+		Interpreter interpreter;
+		try {
+			interpreter = new Interpreter(args[0]);
+			System.out.println("Using DB from file: " + args[0]);
+			
+			try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+				System.out.println("Query your DB (or ':q' to quit): ");
+				for(String q = br.readLine(); !q.equals(":q"); q = br.readLine()) {
+					try {
+		                System.out.println(interpreter.answer(q) ? "SI" : "NO");
+		            } catch (WrongSyntaxException|NonExistentMethodException|RecursiveRuleCallException ex) {
+		                System.err.println(ex.getMessage());
+		            }
+				}
+			}
+			
+		} catch (WrongSyntaxException|RuleJoinException ex) {
+			System.err.println("Error on DB load -> " + ex.getMessage());
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}		
     }
 }
